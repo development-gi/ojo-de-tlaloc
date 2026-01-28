bc7c677793af3856e1bf1387bf9a7e705256bc3e
Said Salas <saidsals10@gmail.com>
2026-01-27 19:23:59 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Ojo de Tlaloc',
+  webDir: 'www'
+};
+
+export default config;
